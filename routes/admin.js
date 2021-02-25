cf84b68eadc4a47ffb261b61f72791a2cd0a76da
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-02-26 02:41:15 +0530

    Express.js End

---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+});
+
+router.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;

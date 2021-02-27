bccf85970e3de612db32242cefb46259494739cf
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-02-27 14:32:26 +0530

    Added cart placeholders

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,13 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+      res.render("shop/product-detail", {product: product, pageTitle: product.title, path: "/products"});
+  });
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
@@ -26,6 +33,12 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
--- models/product.js
+++ models/product.js
@@ -13,7 +13,7 @@ const getProductsFromFile = (cb) => {
   });
 }
 
-module.exports = class Product{
+module.exports = class Product {
   constructor(title, imageUrl, description, price) {
     this.title = title;
     this.imageUrl = imageUrl;
@@ -22,6 +22,7 @@ module.exports = class Product{
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -33,4 +34,11 @@ module.exports = class Product{
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
-}
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};

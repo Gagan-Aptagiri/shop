483d9714b9d817ee63e689802805f06a4dd0597d
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-02-28 13:24:57 +0530

    Added edit product and updated the view

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,16 +4,16 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Adding a Product",
     path: "admin/add-product",
-    editing: false
+    editing: false,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl= req.body.imageUrl;
+  const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
@@ -21,20 +21,37 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
     if (!product) {
-      return res.redirect('/');
+      return res.redirect("/");
     }
     res.render("admin/edit-product", {
       pageTitle: "Editing a Product",
       path: "admin/edit-product",
       editing: editMode,
-      product: product
+      product: product,
     });
   });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const productId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedPrice = req.body.price;
+  const updatedProduct = new Product(
+    productId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -45,4 +62,4 @@ exports.getProducts = (req, res, next) => {
       path: "admin/products",
     });
   });
-}
+};
--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,11 @@ router.get('/add-product', adminController.getAddProduct);
 
 router.get('/products', adminController.getProducts);
 
+router.post('/add-product', adminController.postAddProduct);
+
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
-router.post('/add-product', adminController.postAddProduct);
+router.post('/edit-product', adminController.postEditProduct);
+
 
 module.exports = router;

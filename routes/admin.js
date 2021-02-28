257c069427b9262195e39dd710de17dc7f271de2
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-02-28 11:54:06 +0530

    added edit-product controller

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,8 @@ router.get('/add-product', adminController.getAddProduct);
 
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;

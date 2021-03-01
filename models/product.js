52a3c0b2e813816bd4a794f89c590fc89ab78bb5
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-01 11:36:24 +0530

    Added Delete functionalty

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
+const Cart = require('./cart');
+
 const p = path.join(path.dirname(appRoot), "data", "products.json");
 
 const getProductsFromFile = (cb) => {
@@ -38,6 +40,18 @@ module.exports = class Product {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter((p) => p.id === id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 

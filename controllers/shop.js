d26515031de75c8aeacd26a385faa51f9bb52508
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-02 22:50:45 +0530

    Added order controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -111,16 +110,38 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user
+        .createOrder()
+        .then(order => {
+          return order.addProducts(
+            products.map(product => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            }));
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+      fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "shop/orders",
     pageTitle: "Your Orders",
   });
 };
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: 'shop/checkout',
-    pageTitle: 'checkout'
-  });
-}

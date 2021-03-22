94d77cfc41fa61baecfc1625c0df54afbfaeb4bb
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-23 00:28:25 +0530

    Added pdfkit for invoice generation

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -155,17 +156,56 @@ exports.getInvoice = (req, res, next) => {
 			}
 			const invoiceName = 'invoice-' + orderId + '.pdf';
 			const invoicePath = path.join('data', 'invoices', invoiceName);
-			fs.readFile(invoicePath, (err, data) => {
-				if (err) {
-					return next(err);
-				}
-				res.setHeader('Content-type', 'application/pdf');
-				res.setHeader(
-					'Content-Disposition',
-					'inline; filename="' + invoiceName + '"',
-				);
-				res.send(data);
+
+			const pdfDoc = new PDFDocument();
+			res.setHeader('Content-Type', 'application/pdf');
+			res.setHeader(
+				'Content-Disposition',
+				'inline; filename="' + invoiceName + '"',
+			);
+			pdfDoc.pipe(fs.createWriteStream(invoicePath));
+			pdfDoc.pipe(res);
+			pdfDoc.fontSize(26).text('Invoice', {
+				underline: true,
 			});
+			pdfDoc.text('------------------------------');
+			let totalPrice = 0;
+			order.products.forEach((prod) => {
+				totalPrice += prod.quantity * prod.product.price;
+				pdfDoc
+					.fontSize(14)
+					.text(
+						prod.product.title +
+							' - ' +
+							prod.quantity +
+							' X ' +
+							'$' +
+							prod.product.price,
+					);
+			});
+			pdfDoc.text('--------');
+			pdfDoc.fontSize(20).text('Total Price: $' + totalPrice.toFixed(3));
+			pdfDoc.end();
+			//Method 1: Create in Memory (preload data)
+			// fs.readFile(invoicePath, (err, data) => {
+			// 	if (err) {
+			// 		return next(err);
+			// 	}
+			// 	res.setHeader('Content-type', 'application/pdf');
+			// 	res.setHeader(
+			// 		'Content-Disposition',
+			// 		'inline; filename="' + invoiceName + '"',
+			// 	);
+			// 	res.send(data);
+			// });
+			//Method 2: Create a stream
+			// const file = fs.createReadStream(invoicePath);
+			// res.setHeader('Content-type', 'application/pdf');
+			// res.setHeader(
+			// 	'Content-Disposition',
+			// 	'inline; filename="' + invoiceName + '"',
+			// );
+			// file.pipe(res);
 		})
 		.catch((err) => next(err));
 };

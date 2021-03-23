d21cf56db404e42057baac07b59866036be2fec7
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-23 21:43:33 +0530

    Pagination complete

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,15 +5,31 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+	const page = +req.query.page || 1;
+	let totalItems;
+
 	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then((products) => {
 			res.render('shop/product-list', {
 				prods: products,
-				pageTitle: 'All Products',
+				pageTitle: 'Products List',
 				path: '/products',
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
 			});
 		})
 		.catch((err) => {

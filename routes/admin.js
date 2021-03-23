aa97920f1c8a7b543745c22866cb610d58cf1613
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-24 00:27:02 +0530

    Added a nice async delete request with deleting products

--- routes/admin.js
+++ routes/admin.js
@@ -11,29 +11,29 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 router.get('/products', isAuth, adminController.getProducts);
 
 router.post(
-  '/add-product',
-  [
-    body('title').isString().isLength({ min: 3 }).trim(),
-    body('price').isFloat(),
-    body('description').isLength({ min: 5, max: 400 }).trim(),
-  ],
-  isAuth,
-  adminController.postAddProduct
+	'/add-product',
+	[
+		body('title').isString().isLength({ min: 3 }).trim(),
+		body('price').isFloat(),
+		body('description').isLength({ min: 5, max: 400 }).trim(),
+	],
+	isAuth,
+	adminController.postAddProduct,
 );
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
 router.post(
-  '/edit-product',
-  [
-    body('title').isString().isLength({ min: 3 }).trim(),
-    body('price').isFloat(),
-    body('description').isLength({ min: 5, max: 400 }).trim(),
-  ],
-  isAuth,
-  adminController.postEditProduct
+	'/edit-product',
+	[
+		body('title').isString().isLength({ min: 3 }).trim(),
+		body('price').isFloat(),
+		body('description').isLength({ min: 5, max: 400 }).trim(),
+	],
+	isAuth,
+	adminController.postEditProduct,
 );
 
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 module.exports = router;

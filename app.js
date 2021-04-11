af111e660e66fe88569b606b7987be10a513f12e
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-11 15:14:46 +0530

    Fix: Refactor hard coded connection string to Heroku config var

--- app.js
+++ app.js
@@ -11,37 +11,36 @@ const { v4: uuidv4 } = require('uuid');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
-  'mongodb+srv://gags:gags123@alpha.tlchy.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions',
+	uri: MONGODB_URI,
+	collection: 'sessions',
 });
 
 const csurfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null,'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, uuidv4() + '-' + file.originalname);
-  },
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, uuidv4() + '-' + file.originalname);
+	},
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
 };
 
 app.set('view engine', 'ejs');
@@ -53,42 +52,42 @@ const authRoutes = require('./routes/auth');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
 );
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images',express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	}),
 );
 
 app.use(csurfProtection);
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then((user) => {
-      if (!user) {
-        return next();
-      }
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			if (!user) {
+				return next();
+			}
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			throw new Error(err);
+		});
 });
 
 app.use('/admin', adminRoutes);
@@ -100,22 +99,22 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session.isLoggedIn,
-  });
+	res.status(500).render('500', {
+		pageTitle: 'Error!',
+		path: '/500',
+		isAuthenticated: req.session.isLoggedIn,
+	});
 });
 
 mongoose
-  .connect(MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then((result) => {
-    app.listen(3000);
-    console.log('Connected to the database.');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect(MONGODB_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then((result) => {
+		app.listen(3000);
+		console.log('Connected to the database.');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
